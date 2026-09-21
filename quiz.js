22770c15469d3559e3b245b5bc7b461b57f3ae9d
baipai333 <09971@khamphoempittaya.ac.th>
2026-09-21 16:30:55 +0700

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'verb-quiz-text-v2';
+const ASSETS = [
+    './',
+    './quiz.html',
+    './quiz.json'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
+});
